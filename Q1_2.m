e0a23a4c1d43e80f7280cf0062af6d6dc5956ef7
HoseinAsghari <109804632+HoseinAsghari@users.noreply.github.com>
2023-07-04 18:59:52 +0330

    added Q1_2.m

---
+++ Q1_2.m
@@ -0,0 +1,32 @@
+clc;clear all;
+f=imread("q1.jpg");
+f2=fft2(f);
+F=fftshift(f2,3);% it brings dc point to the center and in image dc point are purely white
+[m,n,c]=size(f);%get size of matrix related to image
+[u,v] = meshgrid(-m/2:m/2-1,-n/2:n/2-1);
+k=1e-6;
+H_equivalent=4*(pi^2).*(u.^2 + v.^2);
+H_equivalent=fftshift(H_equivalent);
+ h=uint8(abs(k*ifft2(H_equivalent.*F)));
+ new_a=f+h;
+  new_a_shift=ifftshift((new_a),3);
+  % BAD STATE
+  k=1e-5;
+H_equivalent=4*(pi^2).*(u.^2 + v.^2);
+H_equivalent=fftshift(H_equivalent);
+ h=uint8(abs(k*ifft2(H_equivalent.*F)));
+new_a_bad=f+h;
+  new_a_bad_shift=ifftshift((new_a_bad),3);
+  %% plot
+  figure(1)
+  subplot(1,3,2)
+ imshow((new_a));
+ title('sharpend by k=1e-6')
+  subplot(1,3,1)
+ imshow((f));
+ title('Original')
+  subplot(1,3,3)
+ imshow((new_a_bad));
+ title('sharpend by k=1e-5')
+%% writing images
+imwrite((new_a),'q1-res3.jpg');
